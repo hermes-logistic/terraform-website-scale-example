3bee413eb1d246d081611bdbef8f76602b8bbf42
Rafael <ralfaro@hermesv.io>
2024-09-28 17:26:55 -0600

    add service cloudresourcemanager

--- website/services.tf
+++ website/services.tf
@@ -1,4 +1,19 @@
+resource "google_project_service" "cloudresourcemanager" {
+  project = data.terraform_remote_state.project.outputs.project_id
+  service = "cloudresourcemanager.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
 resource "google_project_service" "usage" {
+  depends_on = [
+    google_project_service.cloudresourcemanager
+   ]
   project = data.terraform_remote_state.project.outputs.project_id
   service = "serviceusage.googleapis.com"
 
