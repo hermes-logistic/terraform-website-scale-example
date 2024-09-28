418e2e1104e9c70ea9c2e1d1bdc7a7fa08ff44f0
Rafael <ralfaro@hermesv.io>
2024-09-28 17:10:55 -0600

    add enable api service and depends on to google_service_account

--- website/service_accounts.tf
+++ website/service_accounts.tf
@@ -1,4 +1,7 @@
 resource "google_service_account" "spa" {
+  depends_on = [
+    google_project_service.iam
+  ]
   account_id   = join("-", [local.project_name, local.environment])
   display_name = "Service Account for Cloud Run website"
 }
