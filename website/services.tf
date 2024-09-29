e482a4c7d56f46cefae12af37be6c3735f1f3c04
Rafael <ralfaro@hermesv.io>
2024-09-29 15:38:31 -0600

    remove service iam

--- website/services.tf
+++ website/services.tf
@@ -1,15 +1,3 @@
-resource "google_project_service" "iam" {
-  project = data.terraform_remote_state.project.outputs.project_id
-  service = "iam.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
-}
-
 resource "google_project_service" "run" {
   depends_on = [
     google_project_service.iam,
