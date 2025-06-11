a3ecda57d74e3c966840fa5d5231d8f53c7c71e6
Tariq <tventura@hermesv.io>
2025-06-11 17:33:21 -0600

    enbale sql api

--- project/services.tf
+++ project/services.tf
@@ -84,3 +84,18 @@ resource "google_project_service" "registry" {
 
   disable_dependent_services = true
 }
+
+resource "google_project_service" "registry" {
+  depends_on = [
+    google_project_service.registry
+  ]
+  project = google_project.project.project_id
+  service = "sqladmin.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
