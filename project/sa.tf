5c96d745f3582aeae8ff7839a680fe949f9f9de7
Tariq Ventura <tariqventura@gmail.com>
2025-10-31 11:29:03 -0600

    enable appengine

--- project/sa.tf
+++ project/sa.tf
@@ -100,3 +100,11 @@ resource "google_project_iam_member" "kc-sa-admin" {
   member   = "serviceAccount:${google_service_account.network["keycloak"].email}"
   depends_on = [google_service_account.network]
 }
+
+resource "google_project_iam_member" "kc-projectiam-admin" {
+  count    = contains(values(var.services)[*].name, "keycloak") ? 1 : 0
+  project  = google_project.project.project_id
+  role     = "roles/resourcemanager.projectIamAdmin"
+  member   = "serviceAccount:${google_service_account.network["keycloak"].email}"
+  depends_on = [google_service_account.network]
+}
